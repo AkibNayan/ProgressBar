50e9b744732ebdd3a7562e378ba2eadffb8c086a
Akib Uddin Nayan <akibnayan182@gmail.com>
2021-05-22 06:47:07 -0700

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,41 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#1abc9c'
+    });
+    $('.css').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#2ecc71'
+    });
+
+
+
+    $('.php').rProgressbar({
+        percentage: 10,
+        fillBackgroundColor: '#9b59b6'
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 30,
+        fillBackgroundColor: '#34495e'
+    });
+
+
+
+    $('.jQuery').rProgressbar({
+        percentage: 40,
+        fillBackgroundColor: '#f1c40f',
+        backgroundColor: '#e67e22'
+    });
+
+
+
+
+
+
+
+})(jQuery);
